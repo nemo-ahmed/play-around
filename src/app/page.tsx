aeb513d2169cdb8819a37cb622575090091f912b
ahmed <nemo_ahmed@hotmail.com>
2025-02-28 16:28:08 +0000

    cleaning

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
 'use client'
-import useUSDaily from '@/hooks/useUSDaily'
 import Link from 'next/link'
 
 export default function Home() {
-  useUSDaily()
   return (
     <div className="h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="">
