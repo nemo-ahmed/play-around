5ab40ab758b745f3adb56ea3016c3b4a29e31cfb
ahmed <nemo_ahmed@hotmail.com>
2025-03-11 00:34:51 +0000

    infinite scroll removing from the other end

--- src/hooks/useInfiniteScroll.ts
+++ src/hooks/useInfiniteScroll.ts
@@ -5,23 +5,47 @@ const MIN_ON_DISPLAY_DATA_SIZE = 100
 const INCREMENT_BY = 12
 
 function useInfiniteScroll<T = unknown>({data}: {data: T[]}) {
-  const [current, setCurrent] = useState(100)
-  const [displayData, setDisplayData] = useState(data.slice(0, current))
+  const [start, setStart] = useState(0)
+  const [end, setEnd] = useState(100)
+  const [displayData, setDisplayData] = useState(data.slice(0, end))
   const onScroll = useCallback<UIEventHandler<HTMLTableSectionElement>>(
     e => {
       if (data.length <= MIN_ON_DISPLAY_DATA_SIZE) return
-      if (
+      const scrollProgress =
         (e.currentTarget.scrollTop + e.currentTarget.offsetHeight) /
-          e.currentTarget.scrollHeight >
-        0.75
-      ) {
-        setDisplayData(prev => {
-          return prev.concat(data.slice(current, current + INCREMENT_BY))
-        })
-        setCurrent(prev => prev + INCREMENT_BY)
+        e.currentTarget.scrollHeight
+
+      if (scrollProgress < 0.25 && end - MAX_ON_DISPLAY_DATA_SIZE > 0) {
+        const currentOtherEnd = end - MAX_ON_DISPLAY_DATA_SIZE
+        const newStart = start - INCREMENT_BY
+        const newEnd = end - INCREMENT_BY
+        if (currentOtherEnd <= 0) return
+        setDisplayData(
+          data.slice(
+            newStart <= 0 ? 0 : newStart,
+            newEnd > MAX_ON_DISPLAY_DATA_SIZE
+              ? newEnd
+              : MAX_ON_DISPLAY_DATA_SIZE,
+          ),
+        )
+        if (newStart > 0) {
+          setStart(newStart)
+        }
+
+        if (newEnd > MAX_ON_DISPLAY_DATA_SIZE) {
+          setEnd(newEnd)
+        }
+      }
+
+      if (scrollProgress > 0.75) {
+        setDisplayData(data.slice(start, end + INCREMENT_BY))
+        setEnd(prev => prev + INCREMENT_BY)
+        if (displayData.length > MAX_ON_DISPLAY_DATA_SIZE) {
+          setStart(prev => prev + INCREMENT_BY)
+        }
       }
     },
-    [current, data],
+    [data, end, start, displayData.length],
   )
   return {data: displayData, onScroll}
 }
