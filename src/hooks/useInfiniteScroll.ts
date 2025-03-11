4d7ca342271ca42b655dec969581fc6f8606fb16
ahmed <nemo_ahmed@hotmail.com>
2025-03-11 00:41:16 +0000

    adding pagination request handling;

--- src/hooks/useInfiniteScroll.ts
+++ src/hooks/useInfiniteScroll.ts
@@ -4,13 +4,22 @@ const MAX_ON_DISPLAY_DATA_SIZE = 300
 const MIN_ON_DISPLAY_DATA_SIZE = 100
 const INCREMENT_BY = 12
 
-function useInfiniteScroll<T = unknown>({data}: {data: T[]}) {
+function useInfiniteScroll<T = unknown>({
+  data,
+  total,
+  isFetching,
+}: {
+  data: T[]
+  total?: number
+  isFetching?: boolean
+}) {
   const [start, setStart] = useState(0)
   const [end, setEnd] = useState(100)
   const [displayData, setDisplayData] = useState(data.slice(0, end))
+  const stackTotal = total ?? data.length
   const onScroll = useCallback<UIEventHandler<HTMLTableSectionElement>>(
     e => {
-      if (data.length <= MIN_ON_DISPLAY_DATA_SIZE) return
+      if (data.length <= MIN_ON_DISPLAY_DATA_SIZE || isFetching) return
       const scrollProgress =
         (e.currentTarget.scrollTop + e.currentTarget.offsetHeight) /
         e.currentTarget.scrollHeight
@@ -37,15 +46,18 @@ function useInfiniteScroll<T = unknown>({data}: {data: T[]}) {
         }
       }
 
-      if (scrollProgress > 0.75) {
-        setDisplayData(data.slice(start, end + INCREMENT_BY))
-        setEnd(prev => prev + INCREMENT_BY)
+      if (scrollProgress > 0.75 && end < stackTotal) {
+        const newEnd = end + INCREMENT_BY
+        setDisplayData(
+          data.slice(start, newEnd < stackTotal ? newEnd : stackTotal),
+        )
+        setEnd(newEnd < stackTotal ? newEnd : stackTotal)
         if (displayData.length > MAX_ON_DISPLAY_DATA_SIZE) {
           setStart(prev => prev + INCREMENT_BY)
         }
       }
     },
-    [data, end, start, displayData.length],
+    [data, isFetching, end, stackTotal, start, displayData.length],
   )
   return {data: displayData, onScroll}
 }
